f79234af21b5e1da089a49867e7ca555ccd30533
Willian Dias <willian.dias360@gmail.com>
2019-10-08 13:32:20 -0300

    Integraão com Hostgator

--- src/App.vue
+++ src/App.vue
@@ -91,7 +91,16 @@
         <div class="card">
           <div class="card-body">
             <ul class="list-group">
-              <li class="list-group-item" v-for="domain in domains" v-bind:key="domain">{{ domain }}</li>
+              <li class="list-group-item" v-for="domain in domains" v-bind:key="domain.name">
+                <div class="row">
+                  <div class="col-md">{{ domain.name }} </div>
+                  <div class="col-md text-right">
+                    <a class="btn btn-info" v-bind:href="domain.checkout" target="_blank">
+                      <span class="fa fa-shopping-cart"></span>
+                    </a>
+                  </div>
+                </div>
+              </li>
             </ul>
           </div>
         </div>
@@ -105,54 +114,49 @@ import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 
 export default {
-	name: "app",
-	data: function() {
-		return {
-			prefix: "",
-			sufix: "",
-			prefixes: ["Air", "Jet", "Flight"],
-			sufixes: ["Hub", "Station", "Mart"],
-			domains: [
-				"AirHub",
-				"AirStation",
-				"AirMart",
-				"JetHub",
-				"JetStation",
-				"JetMart",
-				"FlightHub",
-				"FlightStation",
-				"FlightMart"
-			]
-		};
-	},
-	methods: {
-		addPrefix(prefix) {
-			this.prefixes.push(prefix);
-			this.prefix = "";
-			this.generate();
-		},
-		addSufix(sufix) {
-			this.sufixes.push(sufix);
-			this.sufix = "";
-			this.generate();
-		},
-		generate() {
-			this.domains = [];
-			for (const prefix of this.prefixes) {
-				for (const sufix of this.sufixes) {
-					this.domains.push(prefix + sufix);
-				}
-			}
-		},
-		deletePrefix(prefix){
-			this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
-			this.generate();
-		},
-		deleteSufix(sufix){
-			this.sufixes.splice(this.sufixes.indexOf(sufix),1);
-			this.generate();
-		}
-	}
+  name: "app",
+  data: function() {
+    return {
+      prefix: "",
+      sufix: "",
+      prefixes: ["Air", "Jet", "Flight"],
+      sufixes: ["Hub", "Station", "Mart"]
+    };
+  },
+  methods: {
+    addPrefix(prefix) {
+      this.prefixes.push(prefix);
+      this.prefix = "";
+    },
+    addSufix(sufix) {
+      this.sufixes.push(sufix);
+      this.sufix = "";
+    },
+    deletePrefix(prefix) {
+      this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
+    },
+    deleteSufix(sufix) {
+      this.sufixes.splice(this.sufixes.indexOf(sufix), 1);
+    }
+  },
+  computed: {
+    domains() {
+      console.log("generating domains");
+      const domains = [];
+      for (const prefix of this.prefixes) {
+        for (const sufix of this.sufixes) {
+          const name = prefix + sufix;
+          const url = name.toLowerCase();
+          const checkout = `https://checkout.hostgator.com.br/?a=add&sld=${url}&tld=.com.br`;
+          domains.push({
+            name,
+            checkout
+          });
+        }
+      }
+      return domains;
+    }
+  }
 };
 </script>
 
